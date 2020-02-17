1f59de9b5206b7792886992b894b137d694eb8e5
dragonite <xiaoquanjin@hotmail.com>
2020-02-17 10:37:24 +0800

    add base code

---
+++ Web-Request-Utility/Web-Request-Utility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Request_Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
