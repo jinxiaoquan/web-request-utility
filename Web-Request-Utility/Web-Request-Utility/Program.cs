a395475df0305649bcd29212925a497fc99e3759
dragonite <xiaoquanjin@hotmail.com>
2020-03-03 14:23:13 +0800

    AcceptAllCertifications

--- Web-Request-Utility/Web-Request-Utility/Program.cs
+++ Web-Request-Utility/Web-Request-Utility/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,13 @@ namespace Web_Request_Utility
     {
         static void Main(string[] args)
         {
+            ServicePointManager.ServerCertificateValidationCallback = AcceptAllCertifications;
         }
+
+        static bool AcceptAllCertifications(object sender, X509Certificate certification, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
     }
 }
