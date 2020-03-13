19d46bf06cd81a499b2767b2beab57a2ef7d6de2
dragonite <xiaoquanjin@hotmail.com>
2020-03-13 09:37:25 +0800

    add user name password auth

--- Web-Request-Utility/Web-Request-Utility/HttpRequest/Get.cs
+++ Web-Request-Utility/Web-Request-Utility/HttpRequest/Get.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +19,30 @@ namespace Web_Request_Utility.HttpRequest
             var response = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result;
             var content = response.Content.ReadAsStringAsync().Result;
         }
+
+        public static void Run1(string url, string domain, string userName, string password)
+        {
+            var endpointRequest = WebRequest.Create(url);
+            endpointRequest.Method = "GET";
+            endpointRequest.ContentType = "application/json";
+            endpointRequest.Credentials = new NetworkCredential(userName, password, domain);
+            try
+            {
+                var webResponse = endpointRequest.GetResponse();
+                var webStream = webResponse.GetResponseStream();
+                if (webStream != null)
+                {
+                    var responseReader = new StreamReader(webStream);
+                    var response = responseReader.ReadToEnd();
+                    Console.WriteLine(response);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+
     }
 }
